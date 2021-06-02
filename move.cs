0b8821e924b6f61764a17028d0ea9aba38de4a8d
IrinaP344 <71124960+IrinaP344@users.noreply.github.com>
2021-06-02 09:54:56 +0900

    Add files via upload

---
+++ move.cs
@@ -0,0 +1,41 @@
+﻿  wusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	float v;
+	Rigidbody rb;
+	Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+         anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxis("Vertical");
+        rb.velocity = transform.forward * v * 2f;
+
+        if(v!=0)
+        {
+        	anim.SetBool("isWalk", true);
+        }else
+        {
+        	anim.SetBool("isWalk", false);
+        }
+       
+      	if(Input.GetMouseButtonDown(0))
+      	{
+      		anim.SetTrigger("attack");
+      	}
+
+    }
+    public void PlayVictory()
+      	{
+      		anim.SetTrigger("victory");
+      	}
+}
